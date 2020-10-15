7a1aa757aed33cfe0b28772db4f199fbeb3aa56c
StillR <DavletshinRoman1995@yandex.ru>
2020-10-16 01:44:24 +0300

    deleted

--- Python/Homeworks/Homework_16.10.2020/Homework_15.10.2020_1.py
+++
@@ -1,5 +0,0 @@
-with open('My_file_1.txt', 'x+') as my_file:
-    user_input = "something"
-    while user_input:
-        user_input = input('Введите данные')
-        my_file.writelines(f'{user_input}\n')
